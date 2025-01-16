80d34a1042c9d1a92a3a225fad4bef1d8ccf737c
TSFMarcel <marcel.wierich@tsfct.de>
2025-01-16 16:26:13 +0100

    Create portainer_install.sh

---
+++ portainer_install.sh
@@ -0,0 +1,6 @@
+#bash
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo usermod -aG docker $USER
+docker pull portainer/portainer-ce:latest
+docker run -d -p 9000:9000 --restart always -v /var/run/docker.sock:/var/run/docker.sock portainer/portainer-ce:latest
