0312a21d8d2ca0f1644298cd87b6c89936ad8e2d
TSFMarcel <marcel.wierich@tsfct.de>
2025-01-20 22:49:16 +0100

    Update portainer_install.sh

--- portainer_install.sh
+++ portainer_install.sh
@@ -5,6 +5,3 @@ sudo systemctl start docker
 sudo usermod -aG docker $USER
 docker pull portainer/portainer-ce:latest
 docker run -d -p 8000:8000 -p 9443:9443 --name portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:2.21.5
-wget https://raw.githubusercontent.com/TSFMarcel/tsf_repo/refs/heads/main/ipvlan.sh
-chmod +x ipvlan.sh
-sudo ./ipvlan.sh
