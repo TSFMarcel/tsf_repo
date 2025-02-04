14dc3a55d3451f5f42f2d82275647ea23f3df218
TSFMarcel <marcel.wierich@tsfct.de>
2025-02-04 16:17:48 +0100

    Update nginx-proxy-manager.sh

--- nginx-proxy-manager.sh
+++ nginx-proxy-manager.sh
@@ -10,6 +10,6 @@ docker run -d \
   -v "/var/lib/docker/volumes/npm/letsencrypt:/etc/letsencrypt" \
   docker.io/jc21/nginx-proxy-manager:latest
 echo -e "Der Nginx Proxy Manager wurde gestartet und ist unter \"\033[32mhttp://$(ip a show eth0 | grep 'inet ' | awk '{print $2}' | cut -d/ -f1):81\033[0m\" erreichbar."
-echo "Die Zugangsdaten lauten:"
+echo "Die Default Zugangsdaten lauten:"
 echo -e "Benutzer: \"\033[32madmin@example.com\033[0m\""
 echo -e "Passwort: \"\033[32mchangeme\033[0m\""
