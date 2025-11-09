3d6ae37be019a50a3a30d902513e94965fd544b2
TSFMarcel <marcel.wierich@tsfct.de>
2025-11-09 12:28:24 +0100

    Update first_start_proxmox.sh

--- first_start_proxmox.sh
+++ first_start_proxmox.sh
@@ -1,5 +1,4 @@
 sudo /etc/scripts/portainer_install.sh
 sudo /etc/scripts/ipvlan.sh
 sudo /etc/scripts/cron_job_update_proxmox.sh
-sudo /etc/scripts/netplan_ubuntu.sh
 sudo /etc/scripts/ubuntu_update_proxmox.sh
